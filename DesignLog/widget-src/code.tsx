006125509f2547128b6a23b7bfdf2db469473a02
toqaewa <kath.tokaeva@gmail.com>
2025-03-05 23:04:21 +0300

    feat: created, updated, version + link toggle

--- DesignLog/widget-src/code.tsx
+++ DesignLog/widget-src/code.tsx
@@ -17,10 +17,22 @@ function DesignLog() {
   const [title, setTitle] = useSyncedState("title", "");
   const [description, setDescription] = useSyncedState("description", "");
   const [showDescription, setShowDescription] = useSyncedState("showDescription", true);
+  const [createdAt, setCreatedAt] = useSyncedState("createdAt", "");
   const [version, setVersion] = useSyncedState("version", "1.0.0");
   const [status, setStatus] = useSyncedState("status", "backlog");
   const [link, setLink] = useSyncedState("link", "");
+  const [showLink, setShowLink] = useSyncedState("showLink", true)
   const [logs, setLogs] = useSyncedState<Log[]>("logs", []);
+
+  const lastLogDate = logs.length > 0 
+  ? new Date(Math.max(...logs.map(log => new Date(log.date).getTime()))).toISOString() 
+  : null;
+
+  useEffect(() => {
+    if (!createdAt) {
+      setCreatedAt(new Date().toISOString());
+    }
+  });
 
   // "Кнопка" как AutoLayout с onClick
   const Button = ({
@@ -71,9 +83,20 @@ function DesignLog() {
         </svg>
         `,
       },
+      {
+        itemType: "toggle",
+        propertyName: "toggleLink",
+        tooltip: "Показывать ссылку",
+        isToggled: showLink,
+        icon: `<svg width="22" height="22" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M10 13C10.4295 13.5741 10.9774 14.0491 11.6066 14.3929C12.2357 14.7367 12.9315 14.9411 13.6467 14.9923C14.3618 15.0435 15.0796 14.9403 15.7513 14.6897C16.4231 14.4392 17.0331 14.047 17.54 13.54L20.54 10.54C21.4508 9.59695 21.9548 8.33394 21.9434 7.02296C21.932 5.71198 21.4061 4.45791 20.479 3.53087C19.5519 2.60383 18.2978 2.07799 16.9868 2.0666C15.6758 2.0552 14.4128 2.55918 13.47 3.46997L11.75 5.17997C11.1488 5.78117 10.7727 6.57194 10.6855 7.41299C10.5983 8.25404 10.8052 9.09633 11.27 9.79997M14 11C13.5705 10.4259 13.0226 9.95087 12.3934 9.60705C11.7643 9.26323 11.0685 9.05888 10.3533 9.00766C9.63821 8.95643 8.92041 9.05963 8.24866 9.3102C7.57691 9.56077 6.96689 9.95296 6.46001 10.46L3.46001 13.46C2.54922 14.403 2.04524 15.666 2.05663 16.977C2.06803 18.288 2.59387 19.542 3.52091 20.4691C4.44795 21.3961 5.70202 21.922 7.013 21.9334C8.32398 21.9448 9.58699 21.4408 10.53 20.53L12.24 18.82C12.8412 18.2188 13.2173 17.428 13.3045 16.587C13.3917 15.746 13.1848 14.9036 12.72 14.2" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+        `,
+      },
     ],
     ({ propertyName }) => {
       if (propertyName === "toggleDescription") setShowDescription(!showDescription);
+      if (propertyName === "toggleLink") setShowLink(!showLink);
     }
   );
 
@@ -116,6 +139,50 @@ function DesignLog() {
           width="fill-parent"
         />
       )}
+      {showLink && (
+        <Input
+          value={link}
+          placeholder="Ссылка на макет"
+          onTextEditEnd={(e) => setLink(e.characters)}
+          width="fill-parent"
+        />
+      )}
+      <AutoLayout
+        direction="horizontal"
+        spacing={16}
+      >
+        <AutoLayout
+          direction="vertical"
+          spacing={4}
+          width={"hug-contents"}
+        >
+          <Text fontSize={12}>Created</Text>
+          <Text>{createdAt ? new Date(createdAt).toLocaleString() : "—"}</Text>
+        </AutoLayout>
+
+        <AutoLayout
+          direction="vertical"
+          spacing={4}
+          width={"hug-contents"}
+        >
+          <Text fontSize={12}>Last Update</Text>
+          <Text>{lastLogDate ? new Date(lastLogDate).toLocaleString() : "—"}</Text>
+        </AutoLayout>
+
+        <AutoLayout
+          direction="vertical"
+          spacing={4}
+          width={"hug-contents"}
+        >
+          <Text fontSize={12}>Version</Text>
+          <Input
+            value={version}
+            placeholder="1.0.0"
+            onTextEditEnd={(e) => setVersion(e.characters)}
+            width="fill-parent"
+          />
+        </AutoLayout>
+      </AutoLayout>
       <Button onClick={addLog}>Добавить лог</Button>
 
       {logs.map((log) => (
